752a8edd607948bee6479d82d812e0d22115f4dc
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2021-03-05 12:28:41 -0800

    Update suggestion.js

--- commands/suggestion.js
+++ commands/suggestion.js
@@ -11,7 +11,7 @@ module.exports = {
 
         const member = "384571447496146955"
 
-        const sugchannel = member.guild.channels.cache.find(813916514901295124)
+        const sugchannel = member.guild.channel.cache.find(813916514901295124)
         if(!sugchannel) return message.channel.send("Suggestion channel does not exsit");
         if(!args[0]) return message.channel.send(newEmbed)
 
@@ -29,6 +29,6 @@ module.exports = {
             throw err;
         })
 
-        message.channel.send("You suggestion has been sent")
+        message.channel.send("Your suggestion has been sent")
     }
 }
