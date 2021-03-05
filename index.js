0a7806bd2f77f7443d61fd2e4712a1315521aec1
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2021-03-04 18:55:21 -0800

    1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+const mongoose = require("mongoose");
 const fs = require('fs'); 
 
 client.commands = new Discord.Collection();
