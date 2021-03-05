a63b9234d35707d83271d7f4dfc66eeb55d09aaf
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2021-03-05 15:21:44 -0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: "gives you the server ping",
     execute(message, args, cmd, client, Discord, profileData){
-        message.channel.send('Pong! (~ ' + client.ping + 'ms)');
+        message.channel.send(`Pong! Client ping is ${client.ping} ms`); 
     }
 }
