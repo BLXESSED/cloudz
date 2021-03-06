298194187962f21769f22dd2fa1121b67c8db772
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2021-03-05 20:08:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ client.events = new Discord.Collection();
 client.once('ready', () => {
     client.user.setActivity('c!help | Beta Version | Full Release Upcoming | Low Latency and 100% Free!!! | Created by BLXESSED#3960', {type: 'LISTENING'});
     console.log(`cloudz has ${client.users.cache.size} users, in ${client.guilds.cache.size} servers!`);
-    client.channels.get("817565865343713292").send("✔️ Restart Complete!\n The new update has been recieved and deployed\n\nInvite me using the link below:\nhttps://discord.com/oauth2/authorize?client_id=809138212488478730&scope=bot");
+    const startChannel = client.channels.cache.get('817565865343713292')
+    startChannel.send('✔️ Restart Complete!\n The new update has been recieved and deployed\n\nInvite me using the link below:\nhttps://discord.com/oauth2/authorize?client_id=809138212488478730&scope=bot')
 });
 
 mongoose.connect(process.env.MONGODB_SRV, {
