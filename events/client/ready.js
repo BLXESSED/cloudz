1f2ed1edc3e3cb9cd23b7bcbd5178cb8a0142859
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2021-03-05 17:06:01 -0800

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,4 @@
 module.exports = async () => {
+    bot.channels.get("817562482800984084").send("✔️ Restart Complete!\n The new update has been recieved and deployed\n\nInvite me using the link below:\nhttps://discord.com/oauth2/authorize?client_id=809138212488478730&scope=bot");
     console.log("cloudz is online!");
 }
