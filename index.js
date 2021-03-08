84752bfbcb24f493242457eac58a83bb63ab0cd4
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2021-03-07 18:38:22 -0800

    1

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 const mongoose = require("mongoose");
-const fs = require('fs'); 
+const fs = require('fs');
+const DBL = require("top.gg");
+const dbl = new DBL(process.env.TOPGG_TOKEN, client);
+const dblwebhook = new DBL(process.env.TOPGG_TOKEN, { webhookPort: 8000, webhookAuth: process.env.PASSWORD }); 
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
@@ -26,5 +29,20 @@ mongoose.connect(process.env.MONGODB_SRV, {
 }).catch((err) =>{
     console.log(err);
 })
+
+dbl.on('posted', () => {
+    console.log('Server count posted!');
+  })
+   
+dbl.on('error', e => {
+   console.log(`Oops! ${e}`);
+  })
+
+dblwebhook.webhook.on('ready', hook => {
+    console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
+  });
+dblwebhook.webhook.on('vote', vote => {
+    console.log(`User with ID ${vote.user} just voted!`);
+  });
 
 client.login(process.env.TOKEN);
