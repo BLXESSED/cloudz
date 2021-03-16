6166da75557e8c5a88ebfc7b5414b6f59a151eb7
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2021-03-15 17:11:50 -0700

    1

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 const mongoose = require("mongoose");
 const fs = require('fs');
-const profileModel = require('./models/profileSchema');
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
@@ -27,43 +26,5 @@ mongoose.connect(process.env.MONGODB_SRV, {
 }).catch((err) =>{
     console.log(err);
 })
-
-const Topgg = require("@top-gg/sdk");
-const express = require("express");
-
-const AutoPoster = require('topgg-autoposter')
-
-const ap = AutoPoster(process.env.TOPGG_TOKEN, client)
-
-ap.on('posted', () => {
-  console.log('Posted stats to Top.gg!')
-})
-
-const app = express();
-
-const webhook = new Topgg.Webhook(process.env.PASSWORD);
-
-app.post("/dblwebhook", webhook.middleware(), async (req, res) => {
-  // req.vote will be your vote object, e.g
-  console.log(req.vote.user); // 395526710101278721 < user who voted
-  try{
-    const randomNumber = Math.floor(Math.random() * 10000) + 1000;
-    const player = req.vote.user
-    await profileModel.findOneAndUpdate({
-        userID: player
-    }, {
-        $inc: {
-            bank: randomNumber,
-        }
-    } 
-  );
-
-  client.users.cache.get(player).send(`Thank you for voting. Here is **${randomNumber} coins** as a reward. I have deposited it into your bank`);
-  }catch(err){
-    console.log(err)
-  }
-});
-
-app.listen(process.env.PORT);
 
 client.login(process.env.TOKEN);
---
+++ voterequest.js
@@ -0,0 +1,39 @@
+const profileModel = require('./models/profileSchema');
+
+const Topgg = require("@top-gg/sdk");
+const express = require("express");
+
+const AutoPoster = require('topgg-autoposter')
+
+const ap = AutoPoster(process.env.TOPGG_TOKEN, client)
+
+ap.on('posted', () => {
+  console.log('Posted stats to Top.gg!')
+})
+
+const app = express();
+
+const webhook = new Topgg.Webhook(process.env.PASSWORD);
+
+app.post("/dblwebhook", webhook.middleware(), async (req, res) => {
+  // req.vote will be your vote object, e.g
+  console.log(req.vote.user); // 395526710101278721 < user who voted
+  try{
+    const randomNumber = Math.floor(Math.random() * 10000) + 1000;
+    const player = req.vote.user
+    await profileModel.findOneAndUpdate({
+        userID: player
+    }, {
+        $inc: {
+            bank: randomNumber,
+        }
+    } 
+  );
+
+  client.users.cache.get(player).send(`Thank you for voting. Here is **${randomNumber} coins** as a reward. I have deposited it into your bank`);
+  }catch(err){
+    console.log(err)
+  }
+});
+
+app.listen(process.env.PORT);
