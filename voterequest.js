dc72d8f52d04efd264bc3c00fc7265d3fdf4d13b
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2021-03-15 19:01:29 -0700

    Update voterequest.js

--- voterequest.js
+++ voterequest.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+const client = new Discord.Client
 
 const mongoose = require("mongoose");
 const profileModel = require('./models/profileSchema');
