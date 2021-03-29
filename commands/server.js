1bb6a630bfe50b9a3c86b96af0a891b5040497c5
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2021-03-28 21:34:59 -0700

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -5,7 +5,7 @@ module.exports = {
 
         const newEmbed = new Discord.MessageEmbed()
         .setColor("#FFFFFF")
-        .setDescription("Click [here](https://discord.gg/hTsyPtUQsr) to join out official discord server!\n\nUse this link if you can't use the one above:\nhttps://discord.gg/hTsyPtUQsr")
+        .setDescription("Click [here](https://discord.gg/hg8thzYUk4) to join out official discord server!\n\nUse this link if you can't use the one above:\nhttps://discord.gg/hTsyPtUQsr")
 
         message.channel.send(newEmbed)
     }
