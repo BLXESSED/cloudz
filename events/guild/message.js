77adbb64e039df9559a5f98c34508932cebbb379
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2022-12-11 11:30:43 -0800

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -29,32 +29,36 @@ module.exports = async (Discord, client, message) => {
         }catch(err){
         console.log(err);
     }
-
-    if(!cooldowns.has(command.name)){
-        cooldowns.set(command.name, new Discord.Collection());
-    }
-
-    const current_time = Date.now();
-    const time_stamps = cooldowns.get(command.name);
-    const cooldown_amount = (command.cooldown) * 1000;
-
-    if(time_stamps.has(message.author.id)){
-        const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
-
-        if(current_time < expiration_time){
-            const time_left = (expiration_time - current_time)
-
-            let days = Math.floor(time_left / 86400000);
-            let hours = Math.floor(time_left / 3600000) % 24;
-            let minutes = Math.floor(time_left / 60000) % 60;
-            let seconds = Math.floor(time_left / 1000) % 60;
-      
-            return message.channel.send("You can use `c!" + command.name + "` in " + `${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds`);
+    try{
+        if(!cooldowns.has(command.name)){
+            cooldowns.set(command.name, new Discord.Collection());
         }
-    }
 
-    time_stamps.set(message.author.id, current_time);
-    setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount)
+        const current_time = Date.now();
+        const time_stamps = cooldowns.get(command.name);
+        const cooldown_amount = (command.cooldown) * 1000;
+
+        if(time_stamps.has(message.author.id)){
+            const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
+
+            if(current_time < expiration_time){
+                const time_left = (expiration_time - current_time)
+
+                let days = Math.floor(time_left / 86400000);
+                let hours = Math.floor(time_left / 3600000) % 24;
+                let minutes = Math.floor(time_left / 60000) % 60;
+                let seconds = Math.floor(time_left / 1000) % 60;
+      
+                return message.channel.send("You can use `c!" + command.name + "` in " + `${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds`);
+            }
+        }
+
+        time_stamps.set(message.author.id, current_time);
+        setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount)
+        
+    }catch(err){
+        console.log(err)
+    }
 
     try {
         command.execute(message, args, cmd, client, Discord, profileData);
